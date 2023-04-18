836794bbc246ebcc168be7ee46f09f1f5441e102
Kanagamani Dhass <121790297+Mohandhass1@users.noreply.github.com>
2023-04-18 23:39:23 +0530

    Dijkstra

--- PAHT_FINDING_WITH_CURSER.py
+++ PAHT_FINDING_WITH_CURSER.py
@@ -1,16 +1,61 @@
-import pygame
-WHITE_COLOER = ()
+#Dijkstra Alog
+import sys
+
+
+class Dijkstra:
+    def __init__(self,graph):
+        self.V = len(graph)
+        self.grid = graph
+        self.Graph = self.Make_graph()
+
+    def Make_graph(self):
+        graph = {i:[] for i in range(len(self.grid))}
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid[i])):
+                if self.grid[i][j] > 0:
+                    graph[i].append((j,self.grid[i][j]))
+                    graph[j].append((i,self.grid[j][i]))
+        return graph
+
+    def Dijkstra_Algo(self,sorc):
+        inf = sys.maxsize
+        Dp = [inf for _ in range(self.V)]
+        haset = [False for _ in range(self.V)]
+        Dp[sorc] = 0
+
+        for _ in range(self.V):
+
+            v = self.Get_Min(Dp,haset)
+            haset[v] = True
+
+            for d,w in self.Graph[v]:
+                if Dp[v] + w < Dp[d]:
+                    Dp[d] = Dp[v]+w
+        return Dp
+
+    def Get_Min(self,dp,has):
+        mine = sys.maxsize
+        v = 0
+        for i in range(len(dp)):
+            if has[i] == False and dp[i] < mine:
+                mine = dp[i]
+                v = i
+        return v
 
 
 
-width = 500
-pygame.display.set_mode((width, width))
-def main():
-    run = True
-    while run:
-        for evn in pygame.event.get():
-            if evn.type == pygame.QUIT:
-                run = False
 
-    pygame.quit()
-main()
+
+graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
+        [4, 0, 8, 0, 0, 0, 0, 11, 0],
+        [0, 8, 0, 7, 0, 4, 0, 0, 2],
+        [0, 0, 7, 0, 9, 14, 0, 0, 0],
+        [0, 0, 0, 9, 0, 10, 0, 0, 0],
+        [0, 0, 4, 14, 10, 0, 2, 0, 0],
+        [0, 0, 0, 0, 0, 2, 0, 1, 6],
+        [8, 11, 0, 0, 0, 0, 1, 0, 7],
+        [0, 0, 2, 0, 0, 0, 6, 7, 0]
+        ]
+Dijk = Dijkstra(graph)
+p = Dijk.Dijkstra_Algo(0)
+print(p)
